09133c886129b8ea8a40d0ce4c26e5d231d71682
chriskfwoo <chriskfwoo@gmail.com>
2017-10-12 22:54:47 -0400

    file structure

---
+++ cmake-build-debug/Drivers/CMakeFiles/PlayerDriver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlayerDriver.dir/PlayerDriver.cpp.o"
+  "PlayerDriver.pdb"
+  "PlayerDriver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlayerDriver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/Risk/CMakeFiles/Risk.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chriswoo/Documents/COMP345/Risk/Card.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Card.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Country.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Country.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Deck.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Deck.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/DiceFacility.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/DiceFacility.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Die.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Die.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Hand.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Hand.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Map.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Map.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/MapLoader.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/MapLoader.cpp.o"
+  "/Users/chriswoo/Documents/COMP345/Risk/Player.cpp" "/Users/chriswoo/Documents/COMP345/cmake-build-debug/Risk/CMakeFiles/Risk.dir/Player.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
