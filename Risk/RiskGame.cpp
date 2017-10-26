daa7620ffdc0947932b123b5b17dd99a0b031761
chriskfwoo <chriskfwoo@gmail.com>
2017-10-26 17:45:06 -0400

    output for risk game

--- Risk/RiskGame.cpp
+++ Risk/RiskGame.cpp
@@ -11,16 +11,30 @@ int main() {
 	Game game = Game(mapName, nPlayers);
 
 	//tests part 2. Edits to Country,Player and Game.
-	vector<Player> p = game.getTurns();
+	vector<Player> player = game.getTurns();
 
+    cout<< endl << "Player turn order: ";
+    for (int i = 0; i < player.size(); i++) {
+        cout << " " << player[i].getID();
+    }
 
-	for (int i = 0; i < p.size(); i++)
-	{
-		cout<< p[i].getID() << " has " << p[i].amtCountries() << " countries." << endl;
-        p[i].printCountries();
+    cout << endl;
+	for (int i = 0; i < player.size(); i++) {
+		cout<< " ~ " << player[i].getID() << " has " << player[i].amtCountries() << " countries and " << player[i].getPlayerArmies() << " armies." <<endl;
+        cout << "Countries: ";
+        player[i].printCountries();
+        cout << endl;;
 	}
-	//	cout << "Player a countries: " << endl;
 
+	while(player.size() > 1){
+
+        // iterate player turns
+        for (int i = 0; i < player.size(); i++) {
+
+
+        }
+
+	}
     
 	return 0;
 }
